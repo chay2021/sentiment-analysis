a258a354fdbccdf3e332773570a73ffcdbc1a674
chay2021 <90015057+chay2021@users.noreply.github.com>
2021-11-11 16:31:20 -0600

    Create output.py

---
+++ output.py
@@ -0,0 +1,37 @@
+{
+    "data": {
+        "id": "1457580259134554117",
+        "text": "If India bowl first, they need to chase down Namibia's target in 6 overs (36 balls) and if India bat first, then they must score 180 and win the match by 152 runs to reach home early. #Cricket"
+    },
+    "matching_rules": [
+        {
+            "id": "1457580189068759042",
+            "tag": "T20 world cup"
+        }
+    ]
+}
+{
+    "data": {
+        "id": "1457580164938883072",
+        "text": "@ESPNcricinfo Shaheen's proved it they have a much better side of the World cricket although they face some bad behavior from other teams \ud83d\ude20\ud83d\udcaa"
+    },
+    "matching_rules": [
+        {
+            "id": "1457580189068759042",
+            "tag": "T20 world cup"
+        }
+    ]
+}
+{
+    "data": {
+        "id": "1457580168034324482",
+        "text": "RT @daniel86cricket: If India bowl first, they need to chase down Namibia's target in 6 overs (36 balls) and if India bat first, then they\u2026"
+    },
+    "matching_rules": [
+        {
+            "id": "1457580189068759042",
+            "tag": "T20 world cup"
+        }
+    ]
+}
+…..
